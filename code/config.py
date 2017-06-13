3a9ed5e99a10dae50a43468667b8692fde6df2c9
Pulkit <pb1988ster@gmail.com>
2017-06-13 11:09:24 -0500

    access token
    added data for consumer token june 13

---
+++ code/config.py
@@ -0,0 +1,7 @@
+consumer_key='Px4ILSJY1hsw6SaULKYZbBHaO'
+
+consumer_secret= 'FASn9rwkQ08AE8YqXMzEO8wnZ5XSNS7dC3HueSuROxEl3PP0PO'
+
+access_token= '221763790-mNpvI2pifXZEHexrEDDSAdW9WNaVvArgoVlJSENP'
+
+access_secret= 'TWFc1HVtDMWWk7WBxQLpsIE56oz4FhHRF09iyj5kk8Ohs'
